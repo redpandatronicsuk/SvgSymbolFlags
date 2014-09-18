90edb35a2e83440f23ca3eef57fde0471646bdea
redpandatronicsuk <redpandatronics@gmail.com>
2014-09-18 21:26:44 +0100

    yo angular init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name svgSymbolFlagsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the svgSymbolFlagsApp
+ */
+angular.module('svgSymbolFlagsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
