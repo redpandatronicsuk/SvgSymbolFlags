2d670d2deabbd2be3f13cac13dc6fd43a3bf24da
redpandatronicsuk <redpandatronics@gmail.com>
2014-10-05 04:00:10 +0100

    First commit, but pretty much done.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var flagApp = angular
   .module('svgSymbolFlagsApp', [
     'ngAnimate',
     'ngCookies',
@@ -31,3 +31,12 @@ angular
         redirectTo: '/'
       });
   });
+
+flagApp.run(function ($rootScope) {
+    $rootScope.flagData = [
+        {id: 'ad', code: '<symbol viewBox="0 0 4 3" id="flag-ad"><title>ad</title> <rect height="3" width="1.333" y="0" x="0" fill="#0000bd"/> <rect height="3" width="1.333" y="0" x="1.333" fill="#fff600"/> <rect height="3" width="1.333" y="0" x="2.666" fill="#cc1900"/> </symbol>'},
+        {id: 'de', code: '<symbol viewBox="0 0 4 3" id="flag-de"><title>de</title> <rect height="1" width="4" y="0"/> <rect height="1" width="4" y="1" fill="#f00"/> <rect height="1" width="4" y="2" fill="#ffe600"/> </symbol>'},
+        {id: 'gb', code: '<symbol viewBox="0 0 4 3" id="flag-gb"><title>gb</title> <clippath id="svgstoref9ed9468b8dd37adfef94afe1035541ct"> <path d="M3,1.5 h3 v1.5 z v1.5 h-3 z h-3 v-1.5 z v-1.5 h3 z"/> </clippath> <path d="M0,0 v3 h4 v-3 z" fill="#00247d"/> <path d="M0,0 L4,3 M4,0 L0,3" stroke="#fff" stroke-width=".6"/> <path d="M0,0 L4,3 M4,0 L0,3" stroke="#cf142b" stroke-width=".4"/> <path d="M2,0 v3 M0,1.5 h4" stroke="#fff" stroke-width="1"/> <path d="M2,0 v3 M0,1.5 h4" stroke="#cf142b" stroke-width=".6"/> </symbol>'},
+        {id: 'jp', code: '<symbol viewBox="0 0 4 3" id="flag-jp"><title>jp</title> <rect fill="#fff" height="3" width="4"/> <circle fill="#bc002d" cx="2" cy="1.5" r="1"/> </symbol>'}
+    ];
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,49 @@
  * # MainCtrl
  * Controller of the svgSymbolFlagsApp
  */
-angular.module('svgSymbolFlagsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('svgSymbolFlagsApp')
+ .controller('MainCtrl', function ($scope) {
+ 	$scope.selectedFlags = [];
+ 	$scope.selectedFlagsCode = [];
+
+ 	$scope.toggleSelected = function (id) {
+ 		var idx = $scope.selectedFlags.indexOf(id);
+ 		if (idx == -1) {
+ 			$scope.selectedFlags.push(id);
+ 			$scope.selectedFlagsCode.push($scope.findFlagCode(id));
+ 		} else {
+ 			$scope.selectedFlags.splice(idx, 1);
+ 			$scope.selectedFlagsCode.splice(idx, 1);
+ 		}
+ 	};
+
+ 	$scope.isSelected = function (id) {
+ 		if ($scope.selectedFlags.indexOf(id) == -1) {
+ 			return '';
+ 		} else {
+ 			return 'flaf-cell-selected';
+ 		}
+ 	};
+
+ 	$scope.flagStr = function() {
+ 		if ($scope.selectedFlags.length == 0) {
+ 			return 'Select flags...';
+ 		} else {
+ 			return $scope.selectedFlagsCode;
+ 		}
+ 	};
+
+ 	$scope.findFlagCode = function(id) {
+ 		for (var i = 0; i < $scope.flagData.length; i++) {
+ 			if ($scope.flagData[i].id == id) {
+ 				return $scope.flagData[i].code;
+ 			}
+ 		}
+ 	};
+
+ 	$('.hovery').blurjs({ 
+ 		source: 'body',
+ 		offset: { x: 15, y: -12 },
+ 		overlay: 'rgba(0,100,100,0.1)',
+ 		radius: 9 });
+ });
